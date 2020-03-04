112000698a703678f0e32f446ab9de9c6d1b1a54
Kasper Grubbe <kawsper@gmail.com>
2020-03-04 01:25:08 +0000

    Hooks should be classes, not objects

--- lib/nomade/hooks.rb
+++ lib/nomade/hooks.rb
@@ -1,7 +1,7 @@
 module Nomade
   module Hooks
-    DEPLOY_RUNNING = Object.new
-    DEPLOY_FINISHED = Object.new
-    DEPLOY_FAILED = Object.new
+    DEPLOY_RUNNING = Class.new
+    DEPLOY_FINISHED = Class.new
+    DEPLOY_FAILED = Class.new
   end
 end
