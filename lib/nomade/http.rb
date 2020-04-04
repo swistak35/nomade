2dd3c6a2b9e89537810f2ad9bf99fa5e8af2c255
Kasper Grubbe <kawsper@gmail.com>
2020-04-04 19:53:54 +0100

    Raise http errors in http library

--- lib/nomade/http.rb
+++ lib/nomade/http.rb
@@ -203,24 +203,27 @@ module Nomade
       res = begin
         retries ||= 0
         http.request(req)
-      rescue Timeout::Error, Errno::ETIMEDOUT, Errno::EINVAL, Errno::ECONNRESET, EOFError, Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError, Net::ProtocolError, SocketError
+      rescue Timeout::Error, Errno::ETIMEDOUT, Errno::EINVAL, Errno::ECONNRESET, EOFError, Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError, Net::ProtocolError, SocketError => e
         if retries < total_retries
           retries += 1
           sleep 1
           retry
         else
-          raise
+          raise HttpConnectionError.new("#{e.class.to_s} - #{e.message}")
         end
       end
 
-      raise if res.code != "200"
+      if res.code != "200"
+        raise HttpBadResponse.new("Bad response (not 200) but #{res.code}: #{res.body}")
+      end
+
       if res.content_type != expected_content_type
         # Sometimes the log endpoint doesn't set content_type on no content
         # https://github.com/hashicorp/nomad/issues/7264
         if res.content_type == nil && expected_content_type == "text/plain"
           # don't raise
         else
-          raise
+          raise HttpBadContentType.new("Expected #{expected_content_type} but got #{res.content_type}")
         end
       end
 
